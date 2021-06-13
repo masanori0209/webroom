76805325b553af921d8b962b88fd461328bb0419
masanori0209 <masanorimurakoshi0209@gmail.com>
2021-06-13 10:48:35 +0900

    fix fast api

--- socket/app/main.py
+++ socket/app/main.py
@@ -7,9 +7,14 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {
+        "TEST": "API"
+    }
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+    return {
+        "item_id": item_id,
+        "q": q
+    }
